d5c93a296da5c3dea72a9efd6e96c73a16fe66c9
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-10-11 10:47:24 +0100

    Sprint_1
    Major Updates:
    - Added Player Movements (WASD)
    - Added basic jumping mechanic (Space)
    - Set the initial Camera position
    
    Minor Changes:
    - Changed the color of the background
    - Added color to the player object
    - Added a tempory static arm to the player

---
+++ AINT254 - Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    public float jumpForce = 5;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        // Moves player forwards
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward.normalized * Time.deltaTime * speed;
+        }
+        // Moves player backwards
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= Vector3.forward.normalized * Time.deltaTime * speed;
+        }
+        // Moves player left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= Vector3.right.normalized * Time.deltaTime * speed;
+        }
+        // Moves player right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right.normalized * Time.deltaTime * speed;
+        }
+
+        // player jumps
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up.normalized * Time.deltaTime * jumpForce;
+        }
+    }
+} // Player
