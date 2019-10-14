f5aa8b7d9ac9c91feb4765b14307abbedd2c420c
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-10-14 16:50:29 +0100

    Sprint 4 - Demo or Die Prototype
    Major Updates:
    - Changed character sprite to a tank like turret (tanks movement is now static)
    - Rotate tank turrent using A & D
    - Elevate turret using W & S
    - Shoot projectile using LMB
    - Moved the player position to the middle
    - Changed Camera position to child the turret rotation
    
    Minor Updates:
    - Changed the ground color to green

--- AINT254 - Project/Assets/Scripts/ThrowObject.cs
+++ AINT254 - Project/Assets/Scripts/ThrowObject.cs
@@ -7,31 +7,18 @@ public class ThrowObject : MonoBehaviour
     public Rigidbody throwPrefab;
     public float speed = 5;
 
-    //private bool holdingItem = true;
-
     void Start()
     {
-        throwPrefab.GetComponent<Rigidbody>().useGravity = false;
+
     }
 
     void Update()
     {
-       // if (holdingItem)
-        //{
-            //Rigidbody o = Instantiate(throwPrefab, transform.position, transform.rotation);
-            //o.velocity = transform.forward * speed;
-
-            if (Input.GetMouseButtonDown(0))
-            {
-                //holdingItem = false;
-                throwPrefab.GetComponent<Rigidbody>().useGravity = true;
-                //Instantiate(throwPrefab, transform.position, transform.rotation);
-                //throwPrefab.GetComponent<Rigidbody>().AddForce(throwPrefab.position * speed);
-
-                Rigidbody o = Instantiate(throwPrefab, transform.position, transform.rotation);
-                o.velocity = transform.forward * speed;
-
-            }
-       // }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody o = Instantiate(throwPrefab, transform.position, transform.rotation);
+            o.velocity = transform.forward * speed;
+            //TurretControl.instance.TurretRecall();
+        }
     }
 }
---
+++ AINT254 - Project/Assets/Scripts/TurretControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretControl : MonoBehaviour
+{
+    public static TurretControl instance;
+
+    public Transform turret;
+    public Transform barrel;
+    public float eleMin;
+    public float eleMax;
+
+    float elevation = 0;
+    float v;
+
+    public Vector3 prevPos;
+    public Vector3 newPos;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+
+        v = Input.GetAxis("Vertical");
+        elevation = Mathf.Clamp(elevation + v, eleMin, eleMax);
+        turret.localRotation = Quaternion.Euler(elevation, 0, 0);
+    }
+
+    //public void TurretRecall()
+    //{
+    //    barrel.GetComponent<Transform>();
+    //    prevPos = new Vector3(0, 0, barrel.localPosition.z + 1);
+        
+    //    newPos = prevPos(0, 0, barrel.localPosition.z + 1);
+    //}
+}
