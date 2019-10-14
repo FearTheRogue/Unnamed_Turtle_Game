307e40a6d5d9ea6f11c89f37f2465db8d5fe319f
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-10-14 18:49:33 +0100

    Sprint 5 - Demo or Die Prototype
    Major Updates:
    - Added an enemy game object
    - Added an Egg (or target) game object
    - Enemy now travels towards target, destroys the target, and then travels towards spawn
    
    Minor Updates:
    - Color change to the tank
    - Added color to Egg
    - Added color to Enemy
    - Added a 'Nest'
    
    Bugs:
    - After Enemy destroys the target, the rotation is affected so the enemy can't move smoothly

---
+++ AINT254 - Project/Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    public Transform target;
+    public Transform enemySpawn;
+    public float speed = 2f;
+
+    public float stopDist = 0.5f;
+
+    public bool targetBool = false;
+    public bool travelToSpawn = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            targetBool = true;
+            travelToSpawn = false;
+
+            if (targetBool)
+            {
+                transform.LookAt(target.position);
+
+                if ((transform.position - target.position).magnitude > stopDist)
+                {
+                    transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+                    Destroy(target.gameObject, 10f);
+                } else
+                {
+                    return;
+                }
+            }
+        }
+
+        if (target == null)
+        {
+            targetBool = false;
+            travelToSpawn = true;
+
+            if (travelToSpawn)
+            {
+                transform.LookAt(enemySpawn.position);
+
+                transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+            }
+        }
+    }
+
+}
