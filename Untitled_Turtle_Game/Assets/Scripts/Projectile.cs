5d25ff52fd41749510ca178bba2d5882ca10f1c9
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-12-03 11:54:40 +0000

    Sprint 12 - Updating projectile
    Major Updates:
    - Added TempTank scene, for testing purposes
    - Added Projectile script that calculates trajectory for a projectile
    
    Minor Updates:
    - Player can now aim with mouse cursor

---
+++ Untitled_Turtle_Game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Original Projectile Script https://www.youtube.com/watch?reload=9&v=03GHtGyEHas
+
+public class Projectile : MonoBehaviour
+{
+
+    public Rigidbody bombPrefab;
+    public GameObject cursor;
+    public Transform firePoint;
+    public LayerMask layer;
+
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        LaunchProjectile();
+    }
+
+    void LaunchProjectile()
+    {
+        Ray camRay = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(camRay, out hit, 100f, layer))
+        {
+            cursor.SetActive(true);
+            cursor.transform.position = hit.point + Vector3.up * 0.1f;
+
+            Vector3 Vo = CalculateVelocity(hit.point, firePoint.position, 1.5f);
+
+            // Set rotation of the cannon
+            transform.rotation = Quaternion.LookRotation(Vo);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Rigidbody obj = Instantiate(bombPrefab, firePoint.position, Quaternion.identity);
+                obj.velocity = Vo;
+            }
+        }
+        else
+        {
+            cursor.SetActive(false);
+        }
+    }
+
+    Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float time)
+    {
+        //  Define the distance X and Y first
+        Vector3 distance = target - origin;
+        Vector3 distanceXZ = distance;
+        distanceXZ.y = 0f;
+
+        // Create a float that represents our distance
+        float Sy = distance.y;
+        float Sxz = distanceXZ.magnitude;
+
+        // Calculate the Velocity
+        float Vxz = Sxz / time;
+        float Vy = Sy / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
+
+        // Return the result
+        Vector3 result = distanceXZ.normalized;
+        result *= Vxz;
+        result.y = Vy;
+
+        return result;
+    }
+
+
+    //public float speed = 20f;
+    //public int preditionStepsPerFrame = 6;
+    //public Vector3 bombVelocity;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    bombVelocity = this.transform.forward * speed;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Vector3 point1 = this.transform.position;
+    //    float stepSize = 1.0f / preditionStepsPerFrame;
+
+    //    for (float step = 0; step < 1; step += stepSize)
+    //    {
+    //        bombVelocity += Physics.gravity * stepSize * Time.deltaTime;
+    //        Vector3 point2 = point1 + bombVelocity * stepSize * Time.deltaTime;
+
+    //        Ray ray = new Ray(point1, point2 - point1);
+    //        if (Physics.Raycast(ray, (point2 - point1).magnitude))
+    //        {
+    //            Debug.Log("Hit" + gameObject.name);
+    //        }
+
+    //        point1 = point2;
+    //    }
+    //    this.transform.position = point1;
+    //}
+
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Vector3 startPos = this.transform.position;
+    //    Vector3 preditedBombVelocity = bombVelocity;
+    //    float stepSize = 0.01f;
+
+    //    for (float step = 0; step < 1; step += stepSize)
+    //    {
+    //        preditedBombVelocity += Physics.gravity * stepSize;
+    //        Vector3 point2 = startPos + preditedBombVelocity * stepSize;
+    //        Gizmos.DrawLine(startPos, point2);
+    //        startPos = point2;
+    //    }
+    //}
+}
---
+++ Untitled_Turtle_Game/Assets/Scripts/TurretAim.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAim : MonoBehaviour
+{
+    public float speed = 5f;
+
+    float pos = 0.0f;
+
+    public GameObject firePoint;
+    public GameObject barrelJoint;
+    public Vector3 trajectory;
+    public float bombSpeed = 20f;
+    public int preditionStepsPerFrame = 6;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseAim();
+
+       
+        //trajectory.x 
+
+    }
+
+    void MouseAim()
+    {
+        // pos += speed * Input.GetAxis("Mouse X");
+
+        //transform.eulerAngles = new Vector3(0.0f, pos, 0.0f);
+
+        //Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //float midPoint = (-transform.position - Camera.main.transform.position).magnitude * 0.5f;
+
+        //transform.LookAt(mouseRay.origin + mouseRay.direction * midPoint);
+        // transform.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Vector3 point1 = firePoint.transform.position;
+
+        trajectory.x = firePoint.transform.position.z;
+
+        Vector3 preditedBombVelocity = trajectory;
+        float stepSize = 0.01f;
+
+        for (float step = 0; step < 1; step += stepSize)
+        {
+            preditedBombVelocity += Physics.gravity * stepSize;
+            Vector3 point2 = point1 + preditedBombVelocity * stepSize;
+            Gizmos.DrawLine(point1, point2);
+            point1 = point2;
+        }
+    }
+}
